2e24eebfc71f059a77d83577a37921670c68705b
Thomas Carreau <thomascarreau7@gmail.com>
2016-12-10 18:10:46 +0100

    Add usethisvimconfig.sh

---
+++ vimfiles/usethisvimconfig.sh
@@ -0,0 +1,16 @@
+echo "Copying VIM dotfiles..."
+cp .vimrc ~
+cp -R .vim/ ~
+if [ $? -ne 0 ]; then
+    echo >&2 "ERROR!"
+    exit 1
+fi
+
+echo "Installing Vundle..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ $? -ne 0 ]; then
+    echo >&2 "ERROR!"
+    exit 1
+fi
+
+echo "Success!"
